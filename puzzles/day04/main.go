0619fdc212b865b33da8ac706c5a11375361decc
Edward Wang <wardzxzxc@gmail.com>
2024-02-05 23:15:57 +0800

    feat: added day04 parseInput and use struct

--- puzzles/day04/main.go
+++ puzzles/day04/main.go
@@ -4,13 +4,23 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	"golang.org/x/exp/slices"
+
+	"github.com/wardzxzxc/aoc-2023/utils"
 )
+
+type Card struct {
+	cardNum      int
+	winningNums  []int
+	selectedNums []int
+}
 
 func main() {
 	part1("input.txt")
+	part2("test.txt")
 }
 
 func part1(fileName string) {
@@ -49,4 +59,50 @@ func part1(fileName string) {
 	}
 
 	fmt.Println(sum)
+}
+
+func part2(fileName string) {
+	allLines := utils.ReadAllLines(fileName)
+	cards := parseInput(allLines)
+	fmt.Println(cards)
+}
+
+func parseInput(allLines []string) []Card {
+	cards := make([]Card, 0)
+
+	for _, line := range allLines {
+		cardNum, _ := strconv.Atoi(strings.TrimSpace(line[5:strings.Index(line, ":")]))
+		winningNumsString := strings.Split(
+			strings.Split(strings.SplitN(line, ":", 2)[1], "|")[0],
+			" ",
+		)
+		selectedNumsString := strings.Split(
+			strings.Split(strings.SplitN(line, ":", 2)[1], "|")[0],
+			" ",
+		)
+
+		winningNums := make([]int, 0)
+		selectedNums := make([]int, 0)
+
+		for _, num := range winningNumsString {
+			if string(num) != "" {
+				winningNum, _ := strconv.Atoi(string(num))
+				winningNums = append(winningNums, winningNum)
+			}
+		}
+
+		for _, num := range selectedNumsString {
+			if string(num) != "" {
+				selectedNum, _ := strconv.Atoi(string(num))
+				selectedNums = append(selectedNums, selectedNum)
+			}
+		}
+
+		cards = append(
+			cards,
+			Card{cardNum: cardNum, winningNums: winningNums, selectedNums: selectedNums},
+		)
+	}
+
+	return cards
 }
